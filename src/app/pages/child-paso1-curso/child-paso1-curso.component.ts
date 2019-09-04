06138c981d465ba36cc7a32559ade04032f93bf2
danielmendez88 <daniboymendez@gmail.com>
2019-09-04 12:49:08 -0500

    modificacion de pagina de paso 1 del curso

--- src/app/pages/child-paso1-curso/child-paso1-curso.component.ts
+++ src/app/pages/child-paso1-curso/child-paso1-curso.component.ts
@@ -1,4 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+// cursos
+import { CursosService } from '../../services/cursos.service';
+// modelo
+import { CategoriaCursos, Cursos } from '../../models/cursos';
+import { MatTableDataSource, MatPaginator, MatSort } from '@angular/material';
+// importamos snackbar
+import { SnackserviceService } from '../../services/snackservice.service';
+
 
 @Component({
   selector: 'app-child-paso1-curso',
@@ -6,10 +14,56 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./child-paso1-curso.component.scss']
 })
 export class ChildPaso1CursoComponent implements OnInit {
+  // checamos la variable
+  catCurso: CategoriaCursos[];
+  cursos: Cursos[];
+   // columnas
+  displayedColumns = ['curso', 'costo', 'detalle'];
+  // datasource
+  datasource = new MatTableDataSource<Cursos>();
+  // paginadores
+  @ViewChild(MatPaginator) paginator: MatPaginator;
+  @ViewChild(MatSort) sort: MatSort;
+  // variable mostrar elemento
+  show = false;
 
-  constructor() { }
+  constructor(
+    private serviceCourse: CursosService,
+    private snack: SnackserviceService) { }
 
   ngOnInit() {
+    this.getCategories();
+    this.serviceCourse.getAllCursos().subscribe(
+      response => {
+      // llemamos la tabla
+      this.datasource.data = response;
+      this.datasource.sort = this.sort;
+      this.datasource.paginator = this.paginator;
+    }, error => {
+      // mandamos un mensaje de error
+      this.snack.showSnackBar(error, 'Error!');
+    });
   }
 
+  // filtrar cursos
+  getCursoByFilter(event) {
+    this.show = true;
+    this.serviceCourse.getCursoPorCategoria(event)
+        .subscribe((success) => {
+          this.datasource.data = success as Cursos[];
+          this.show = false;
+        }, error => {
+          this.show = false;
+          this.snack.showSnackBar(error, 'Error!');
+        });
+  }
+  
+  // obtener categorias
+  getCategories(): void {
+    this.serviceCourse.getCatCurso()
+        .subscribe
+        ((respose: CategoriaCursos[]) => {
+            this.catCurso = respose;
+        });
+  }
 }
